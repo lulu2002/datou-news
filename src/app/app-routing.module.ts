e741624625fa76aeee586ae1565c450dada6f87b
lulu2002 <lose9749@gmail.com>
2020-10-10 17:58:14 +0800

    跨分頁功能完成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {anchorScrolling: 'enabled'})
+    RouterModule.forRoot(routes, {anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled'})
   ],
   exports: [
     RouterModule
--- src/app/pages/topics-page/topics-page.component.ts
+++ src/app/pages/topics-page/topics-page.component.ts
@@ -1,21 +1,31 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Subject, TopicSection, TopicService} from "../../shared/component/topic/topic.service";
+import {AfterContentChecked, AfterContentInit, AfterViewInit, Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {TopicSection, TopicService} from "../../shared/component/topic/topic.service";
 
 @Component({
   selector: 'app-topics-page',
   templateUrl: './topics-page.component.html',
   styleUrls: ['./topics-page.component.scss']
 })
-export class TopicsPageComponent implements OnInit {
+export class TopicsPageComponent implements OnInit, AfterContentChecked {
 
   private topicSection: TopicSection;
+  private fragment: string;
 
-  constructor(private route: ActivatedRoute, private topicService: TopicService) {
+  constructor(private route: ActivatedRoute,
+              private topicService: TopicService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
+    this.route.fragment.subscribe(fragment => this.fragment = fragment);
+  }
 
+  ngAfterContentChecked(): void {
+    try {
+      document.querySelector('#' + this.fragment).scrollIntoView();
+    } catch (e) {
+    }
   }
 
   getTopicSection(): TopicSection {
